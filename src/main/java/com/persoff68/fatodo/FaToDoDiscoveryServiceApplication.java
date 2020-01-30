12653d9a8c08d93b17f794719bf5d60a15c6d70c
Vladimir Perfilev <persoff68@gmail.com>
2020-01-30 08:52:42 +0100

    setup app

--- src/main/java/com/persoff68/fatodo/FaToDoDiscoveryServiceApplication.java
+++ src/main/java/com/persoff68/fatodo/FaToDoDiscoveryServiceApplication.java
@@ -2,8 +2,10 @@ package com.persoff68.fatodo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class FaToDoDiscoveryServiceApplication {
 
     public static void main(String[] args) {
