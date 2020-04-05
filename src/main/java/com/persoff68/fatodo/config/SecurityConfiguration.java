aa1a271758d5cb9f7f7eae7a0b74c4e972924c4a
Vladimir Perfilev <persoff68@gmail.com>
2020-04-05 02:18:31 +0200

    TDM-26 fix security config

--- src/main/java/com/persoff68/fatodo/config/SecurityConfiguration.java
+++ src/main/java/com/persoff68/fatodo/config/SecurityConfiguration.java
@@ -1,17 +1,11 @@
 package com.persoff68.fatodo.config;
 
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder authManager) throws Exception {
-        // redefine method of parent class
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
